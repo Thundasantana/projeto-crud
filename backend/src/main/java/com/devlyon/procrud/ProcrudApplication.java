fbc4aef0dd78f7853b4bf82ec49875271af6c21b
Ednilson Santana <ednilson.sdas@outlook.com>
2023-02-01 22:35:05 -0300

    Backend project created

---
+++ backend/src/main/java/com/devlyon/procrud/ProcrudApplication.java
@@ -0,0 +1,13 @@
+package com.devlyon.procrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devlyon/procrud/ProcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlyon.procrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
