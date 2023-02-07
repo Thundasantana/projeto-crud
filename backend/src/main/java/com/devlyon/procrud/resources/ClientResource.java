97f0989f9ec85502badc59293f9f5bebc0c90bef
Ednilson Santana <ednilson.sdas@outlook.com>
2023-02-07 20:44:35 -0300

    H2 database

--- backend/src/main/java/com/devlyon/procrud/resources/ClientResource.java
+++ backend/src/main/java/com/devlyon/procrud/resources/ClientResource.java
@@ -1,24 +1,27 @@
 package com.devlyon.procrud.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devlyon.procrud.entities.Client;
+import com.devlyon.procrud.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 
+	@Autowired
+	private ClientService service;
+
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "Name", null, null, null, null));
-		list.add(new Client(2L, "Name", null, null, null, null));
+		List<Client> list = service.findAll();
+
 		return ResponseEntity.ok().body(list);
 	}
 
---
+++ backend/src/main/java/com/devlyon/procrud/services/ClientService.java
@@ -0,0 +1,21 @@
+package com.devlyon.procrud.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devlyon.procrud.entities.Client;
+import com.devlyon.procrud.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+
+	public List<Client> findAll() {
+		return repository.findAll();
+	}
+
+}
