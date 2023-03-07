ad68c06625f63b8b946f966ccf99d5b1ad73bb16
Ednilson Santana <ednilson.sdas@outlook.com>
2023-03-07 19:18:32 -0300

    Update client

--- backend/src/main/java/com/devlyon/procrud/services/ClientService.java
+++ backend/src/main/java/com/devlyon/procrud/services/ClientService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,11 +13,11 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devlyon.procrud.dto.ClientDTO;
 import com.devlyon.procrud.entities.Client;
 import com.devlyon.procrud.repositories.ClientRepository;
-import com.devlyon.procrud.services.exceptions.EntityNotFoundException;
+import com.devlyon.procrud.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
 
@@ -28,7 +30,7 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
 	}
 
@@ -38,5 +40,18 @@ public class ClientService {
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		}
+		catch (EntityNotFoundException e) {	
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
 	}
 }
