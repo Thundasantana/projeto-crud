5f8f6f3b1b5b68de789b4b2fa23f0ecefb92082f
Ednilson Santana <ednilson.sdas@outlook.com>
2023-03-07 21:45:50 -0300

    Auditing data

--- backend/src/main/java/com/devlyon/procrud/services/ClientService.java
+++ backend/src/main/java/com/devlyon/procrud/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devlyon.procrud.dto.ClientDTO;
 import com.devlyon.procrud.entities.Client;
 import com.devlyon.procrud.repositories.ClientRepository;
+import com.devlyon.procrud.services.exceptions.DatabaseException;
 import com.devlyon.procrud.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -49,9 +52,20 @@ public class ClientService {
 			entity.setName(dto.getName());
 			entity = repository.save(entity);
 			return new ClientDTO(entity);
-		}
-		catch (EntityNotFoundException e) {	
+		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);	
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new  ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 }
